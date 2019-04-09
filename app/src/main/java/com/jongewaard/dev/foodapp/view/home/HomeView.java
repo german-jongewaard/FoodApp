ddd6afee4a94862ca2f9cb8baa957f2b25303d76
german-jongewaard <germanjongewaard@gmail.com>
2019-04-09 15:48:10 +0200

    new commit

---
+++ app/src/main/java/com/jongewaard/dev/foodapp/view/home/HomeView.java
@@ -0,0 +1,11 @@
+package com.jongewaard.dev.foodapp.view.home;
+
+import java.util.List;
+
+public interface HomeView {
+    void showLoading();
+    void hideLoading();
+    void setMeal(List<Meals.Meal> meal);
+    void setCategory(List<Categories.Category> category);
+    void onErrorLoading(String message);
+}
